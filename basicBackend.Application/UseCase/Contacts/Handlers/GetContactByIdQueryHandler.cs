79c476cffd69ed35e2151de63e6986afdc299f02
Roberto Nobre <cicero@ciandt.com>
2020-05-14 15:20:32 -0300

    Get by id contact use case

---
+++ basicBackend.Application/UseCase/Contacts/Handlers/GetContactByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using basicBackend.Application.Interfaces;
+using basicBackend.Application.UseCase.Contacts.Dto;
+using basicBackend.Application.UseCase.Contacts.Queries;
+
+namespace basicBackend.Application.UseCase.Contacts.Handlers
+{
+    public class GetContactByIdQueryHandler : IRequestHandler<GetContactByIdQuery, ContactDTO>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetContactByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<ContactDTO> Handle(GetContactByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _unitOfWork.Contacts.Get(request.Id);
+            return _mapper.Map<ContactDTO>(result);
+        }
+    }
+}
---
+++ basicBackend.Application/UseCase/Contacts/Queries/GetContactByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using basicBackend.Application.UseCase.Contacts.Dto;
+using MediatR;
+
+namespace basicBackend.Application.UseCase.Contacts.Queries
+{
+    public class GetContactByIdQuery : IRequest<ContactDTO>
+    {
+        public int Id { get; set; }
+    }
+}
